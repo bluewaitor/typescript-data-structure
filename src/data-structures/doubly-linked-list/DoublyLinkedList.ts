3f02a390f081a7db5fbb2f5576ed4add7f4a268f
bluewaitor <405622394@qq.com>
2018-07-17 21:07:09 +0800

    双链表

---
+++ src/data-structures/doubly-linked-list/DoublyLinkedList.ts
@@ -0,0 +1,164 @@
+import Comparator from "../../utils/comparator/Comparator";
+import DoublyLinkedListNode from "./DoublyLinkedListNode";
+
+class DoublyLinkedList {
+  head: DoublyLinkedListNode;
+  tail: DoublyLinkedListNode;
+  compare: Comparator;
+
+  constructor(comparatorFunction?) {
+    this.head = null;
+    this.tail = null;
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  prepend(value) {
+    const newNode = new DoublyLinkedListNode(value, this.head);
+    if (this.head) {
+      this.head.previous = newNode;
+    }
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  append(value) {
+    const newNode = new DoublyLinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+
+    this.tail.next = newNode;
+    newNode.previous = this.tail;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deleteNode = null;
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (this.compare.equal(currentNode.value, value)) {
+        deleteNode = currentNode;
+
+        if (deleteNode === this.head) {
+          this.head = deleteNode.next;
+
+          if (this.head) {
+            this.head.previous = null;
+          }
+
+          if (deleteNode === this.tail) {
+            this.tail = null;
+          }
+        } else if (deleteNode === this.tail) {
+          this.tail = deleteNode.previous;
+          this.tail.next = null;
+        } else {
+          const previousNode = deleteNode.previous;
+          const nextNode = deleteNode.next;
+
+          previousNode.next = nextNode;
+          nextNode.previous = previousNode;
+        }
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return deleteNode;
+  }
+
+  find({value = undefined, callback = undefined}) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+
+    const deleteHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+      this.head.previous = null;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deleteHead;
+  }
+
+  deleteTail() {
+    if (!this.tail) {
+      return null;
+    }
+
+    if (this.head === this.tail) {
+      const deleteTail = this.tail;
+
+      this.head = null;
+      this.tail = null;
+
+      return deleteTail;
+    }
+
+    const deleteTail = this.tail;
+
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+
+    return deleteTail;
+  }
+
+  toArray() {
+    const nodes = [];
+
+    let currentNode = this.head;
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  toString(callback?) {
+    return this.toArray().map(node => node.toString(callback)).toString();
+  }
+}
+
+export default DoublyLinkedList;
---
+++ src/data-structures/doubly-linked-list/DoublyLinkedListNode.ts
@@ -0,0 +1,16 @@
+class DoublyLinkedListNode {
+  value: any;
+  next: DoublyLinkedListNode;
+  previous: DoublyLinkedListNode;
+  constructor(value, next = null, previous = null) {
+    this.value = value;
+    this.next = next;
+    this.previous = previous;
+  }
+
+  toString(callback?) {
+    return callback ? callback(this.value) : `${this.value}`;
+  }
+}
+
+export default DoublyLinkedListNode;
