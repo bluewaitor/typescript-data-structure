7b96278f63800b4e6a66bd32870bbdafb313f9ab
bluewaitor <405622394@qq.com>
2018-07-17 21:10:20 +0800

    export doubly linked list.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export * from './data-structures/linked-list/LinkedList';
-
-export * from './data-structures/linked-list/LinkedListNode';
-
 export * from './utils/comparator/Comparator';
+export * from './data-structures/linked-list/LinkedListNode';
+export * from './data-structures/linked-list/LinkedList';
+export * from './data-structures/doubly-linked-list/DoublyLinkedListNode';
+export * from './data-structures/doubly-linked-list/DoublyLinkedList';
