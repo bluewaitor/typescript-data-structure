64d4d7efced4c50978de07dde89430c6600472b0
bluewaitor <405622394@qq.com>
2018-09-12 22:19:01 +0800

    add graph vertex and graph edge

---
+++ src/data-structures/graph/GraphEdge.ts
@@ -0,0 +1,33 @@
+import GraphVertex from "./GraphVertex";
+
+class GraphEdge {
+  startVertex: GraphVertex;
+  endVertex: GraphVertex;
+  weight: number;
+  constructor(startVertex, endVertex, weight = 0) {
+    this.startVertex = startVertex;
+    this.endVertex = endVertex;
+    this.weight = weight;
+  }
+
+  getKey() {
+    const startVertexKey = this.startVertex.getKey();
+    const endVertexKey = this.endVertex.getKey();
+
+    return `${startVertexKey}_${endVertexKey}`;
+  }
+
+  reverse() {
+    const tmp = this.startVertex;
+    this.startVertex = this.endVertex;
+    this.endVertex = tmp;
+
+    return this;
+  }
+
+  toString() {
+    return this.getKey();
+  }
+}
+
+export default GraphEdge;
