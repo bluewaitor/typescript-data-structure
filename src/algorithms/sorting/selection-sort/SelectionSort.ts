2ee478308ccc5ebe41e356752b9022b6491022b4
bluewaitor <405622394@qq.com>
2018-09-14 17:49:15 +0800

    add selection sort

---
+++ src/algorithms/sorting/selection-sort/SelectionSort.ts
@@ -0,0 +1,29 @@
+import Sort from '../Sort';
+
+export default class SelectionSort extends Sort {
+  sort(originalArray) {
+    const array = [...originalArray];
+
+    for (let i = 0; i < array.length - 1; i += 1) {
+      let minIndex = i;
+
+      this.callbacks.visitingCallback(array[i]);
+
+      for(let j = i + 1; j < array.length; j += 1) {
+        this.callbacks.visitingCallback(array[j]);
+
+        if (this.comparator.lessThan(array[j], array[minIndex])) {
+          minIndex = j;
+        }
+      }
+
+      if (minIndex !== i) {
+        const temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+      }
+    }
+
+    return array;
+  }
+}
